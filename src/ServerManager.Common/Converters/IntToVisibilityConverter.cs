d96011f24923d087b19f594b661907a6dc0e043b
Brett Hewitson <bletch1971@hotmail.com>
2021-08-20 11:24:38 +1000

    GameData and Converter Changes

---
+++ src/ServerManager.Common/Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ServerManagerTool.Common.Converters
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double scaledValue = System.Convert.ToInt32(value);
+            return scaledValue == 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("IntToVisibilityConverter is a OneWay converter.");
+        }
+    }
+}
