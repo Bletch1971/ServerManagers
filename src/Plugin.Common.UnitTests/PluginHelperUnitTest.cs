4a80b089b4f0534508ca0672839c229758065f83
Brett Hewitson <bletch1971@hotmail.com>
2021-12-02 16:46:53 +1000

    New UnitTest Projects

---
+++ src/Plugin.Common.UnitTests/PluginHelperUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerManagerTool.Plugin.Common;
+using ServerManagerTool.Plugin.Common.Lib;
+using System.Collections.Generic;
+
+namespace Plugin.Common.UnitTests
+{
+    [TestClass]
+    public class PluginHelperUnitTest
+    {
+        [TestMethod]
+        public void PluginHelper_HandleAlert_When_SingleLineMessage_Then_Valid()
+        {
+            // Arrange
+            PluginHelper.Instance.SetFetchProfileCallback(FetchProfiles);
+
+            // Act
+            var profileList = PluginHelper.Instance.FetchProfileList();
+
+            // Assert
+            Assert.IsNotNull(profileList);
+            Assert.IsTrue(profileList.Count == 3);
+        }
+
+        public IList<Profile> FetchProfiles()
+        {
+            return new List<Profile>()
+            {
+                new Profile() { ProfileName = "Profile 1", InstallationFolder = @"d:\asmdata\servers\server1" },
+                new Profile() { ProfileName = "Profile 2", InstallationFolder = @"d:\asmdata\servers\server2" },
+                new Profile() { ProfileName = "Profile 3", InstallationFolder = @"d:\asmdata\servers\server3" },
+            };
+        }
+
+    }
+}
---
+++ src/Plugin.Discord.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Plugin.Discord.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bletch1971")]
+[assembly: AssemblyProduct("Plugin.Discord.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d13fc88-9508-4c3a-8ed0-f7bd46e343cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
